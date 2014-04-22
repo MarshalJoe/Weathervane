04597adafed1560998d5829d59a39fe0c9a3f5cc
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-04-21 19:24:08 -0500

    Adds a bunch of stuff

--- cycleText.js
+++ cycleText.js
@@ -6,22 +6,22 @@ $(function() {
 
 	function fadeTextInOut (element, word, time) {
 		setTimeout((function () {
-			$(element).text(word).fadeIn("slow", function () {
-				$(element).fadeOut("slow");
+			$(element).text(word).fadeIn("fast", function () {
+				$(element).fadeOut(750);
 			});
 		}), time)
 	}
 
 	function fadeTextIn (element, word, time) {
 		setTimeout((function () {
-			$(element).text(word).fadeIn("slow");
+			$(element).text(word).fadeIn("fast");
 		}), time)
 	}
 
 	function cycleText (element, array) {
 		var timeStep = 0;
 		for (var i = 0; i < array.length; i++) {
-			timeStep += 1500;
+			timeStep += 1250;
 
 			if (i < (array.length - 1)) {
 				fadeTextInOut(element, array[i], timeStep);
